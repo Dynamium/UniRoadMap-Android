e64d94eebadcf657fe7ae35c640e6a8d0d9d67ca
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-06-04 12:37:36 +0300

    bottom sheet update

--- app/src/main/java/org/dynamium/uniroadmap/MainActivity.kt
+++ app/src/main/java/org/dynamium/uniroadmap/MainActivity.kt
@@ -2,11 +2,32 @@ package org.dynamium.uniroadmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+
+private var mapView: MapView? = null
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(getApplicationContext(), getString(R.string.mapbox_map_token));
+
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+
+// Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+
+            }
+
+        }
     }
 }
